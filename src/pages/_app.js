05e65c27b2ffc02845b5e0858ca545ddf076d0b7
Audrey Lam <audreylam989@gmail.com>
2023-04-22 02:20:02 -0700

    changed default font to inter

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import "../styles/bootstrap.scss"
+import { Inter } from 'next/font/google'
+
+// default font
+const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
