fac7b426f1ce567f0b80425f3491dc1c2ed85256
Nathan Huey <nate.huey4@gmail.com>
2023-04-22 17:05:35 -0700

    feat: landing page

--- src/pages/views/Landing/Landing.jsx
+++ src/pages/views/Landing/Landing.jsx
@@ -1,9 +1,32 @@
-import "@/lib/firebase";
+import React from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import styles from "./Landing.module.scss";
+import Jeep from "@/assets/icons/jeep-svgrepo-com.svg";
+import { Button } from "react-bootstrap";
 
 export default function Landing() {
+  const router = useRouter();
+
+  function handleClick() {
+    router.push("/login");
+  }
+
   return (
-    <div>
-      <h1>Landing</h1>
+    <div className={styles.landing}>
+      <div className={styles.card}>
+        <div className={styles.heading}>
+          <h1>BeepBeepI'mGoingPlaces</h1>
+        </div>
+        <Image src={Jeep} width={100} alt="Jeep" />
+        <Button
+          variant="primary"
+          className={styles.button}
+          onClick={handleClick}
+        >
+          Login/Signup
+        </Button>
+      </div>
     </div>
   );
 }
