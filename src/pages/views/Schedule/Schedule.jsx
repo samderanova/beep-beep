94805a21499d93d92bed9e0cd264954b72883240
Nathan Huey <nate.huey4@gmail.com>
2023-04-23 06:24:52 -0700

    feat: drag calendar

--- src/pages/views/Schedule/Schedule.jsx
+++ src/pages/views/Schedule/Schedule.jsx
@@ -3,6 +3,7 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { Col, Container, Row, Card } from "react-bootstrap";
 import { db } from "@/lib/firebase";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { Button } from "react-bootstrap";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
@@ -18,45 +19,68 @@ export default function Schedule() {
   const handleDateSelect = (selectInfo) => {
     console.log("handleDateSelect ran");
     // let title = prompt('Please enter a new title for your event')
-    let calendarApi = selectInfo.view.calendar
+    let calendarApi = selectInfo.view.calendar;
 
-    calendarApi.unselect() // clear date selection
+    calendarApi.unselect(); // clear date selection
 
     if (eventSelected !== "") {
       const newEvent = {
-        id: eventSelected,
-        title: eventSelected,
+        id: eventSelected.id,
+        title: eventSelected.title,
         start: new Date(selectInfo.startStr),
-        end: new Date(selectInfo.endStr)
-      }
+        end: new Date(selectInfo.endStr),
+      };
       if (!scheduled.includes(newEvent)) {
         calendarApi.addEvent(newEvent);
         setScheduled([...scheduled, newEvent]);
-        setUnscheduled(unscheduled.filter(unscheduledEvent => unscheduledEvent !== eventSelected));
+        setUnscheduled(
+          unscheduled.filter(
+            (unscheduledEvent) => unscheduledEvent !== eventSelected
+          )
+        );
         setEventSelected("");
         console.log(newEvent);
       }
     }
-  }
+  };
 
   const handleEventClick = (clickInfo) => {
-    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+    if (
+      confirm(
+        `Are you sure you want to delete the event '${clickInfo.event.title}'`
+      )
+    ) {
       clickInfo.event.remove();
       const removedEvent = clickInfo.event.title;
-      setScheduled(scheduled.filter(scheduledEvent => scheduledEvent.id !== eventSelected));
+      setScheduled(
+        scheduled.filter(
+          (scheduledEvent) => scheduledEvent.id !== eventSelected.id
+        )
+      );
       setUnscheduled([...unscheduled, removedEvent]);
       setEventSelected("");
     }
-  }
+  };
 
   useEffect(() => {
     onAuthStateChanged(auth, async (user) => {
       if (user) {
+        console.log("in useEffect");
         const uid = user.uid;
         const userRef = doc(db, "users", uid);
         const docSnap = await getDoc(userRef);
-        setScheduled(docSnap.data().scheduled);
+        setScheduled(
+          docSnap.data().scheduled.map((event) => {
+            return {
+              id: event.id,
+              title: event.name,
+              start: new Date(event.start),
+              end: new Date(event.end),
+            };
+          })
+        );
         setUnscheduled(docSnap.data().unscheduled);
+        console.log("use effect done");
       } else {
         console.log("Ooga booga i am not logged in");
       }
@@ -67,16 +91,17 @@ export default function Schedule() {
     console.log("createEvents ran");
     const events = [];
     for (let i = 0; i < scheduled.length; i++) {
+      console.log(scheduled[i]);
       const event = {
         id: scheduled[i].id,
-        title: scheduled[i].id,
+        title: scheduled[i].name,
         start: new Date(scheduled[i].start),
         end: new Date(scheduled[i].end),
       };
       console.log(event);
       events.push(event);
     }
-    console.log(scheduled);
+    console.log(events);
     return events;
   };
 
@@ -84,7 +109,46 @@ export default function Schedule() {
     console.log("setEvents run");
     setScheduled(events);
     console.log(scheduled);
-  }
+  };
+
+  const onSubmit = async () => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const uid = user.uid;
+        const userRef = doc(db, "users", uid);
+        // await updateDoc(userRef, {
+        //   scheduled: scheduled,
+        // });
+        console.log(unscheduled);
+        console.log(scheduled);
+      }
+    });
+  };
+
+  const genCards = () => {
+    const cards = [];
+    for (let i = 0; i < unscheduled.length; i++) {
+      cards.push(
+        <Card
+          key={unscheduled[i].id}
+          style={{
+            backgroundColor:
+              unscheduled[i].id === eventSelected.id ? "lightblue" : "",
+          }}
+          onClick={() =>
+            setEventSelected(
+              eventSelected.id === unscheduled[i].id ? "" : unscheduled[i]
+            )
+          }
+        >
+          {unscheduled[i].name}
+        </Card>
+      );
+    }
+
+    console.log(cards);
+    return cards;
+  };
 
   return (
     <Container fluid className={styles.calendar + " p-5"}>
@@ -102,23 +166,19 @@ export default function Schedule() {
             selectable={true}
             selectMirror={true}
             dayMaxEvents={true}
-            initialEvents={createEvents()}
+            // initialEvents={createEvents()}
+            initialEvents={[{ title: "event 1", date: "2021-09-01" }]}
             select={handleDateSelect}
             eventClick={handleEventClick}
             eventsSet={setEvents}
           />
+          <Button className="mt-3" onClick={onSubmit}>
+            Submit
+          </Button>
         </Col>
         <Col>
           <h1>Schedule</h1>
-          {unscheduled.map((unselectedEvent) => 
-            <Card 
-              key={unselectedEvent} 
-              style={{
-                backgroundColor: unselectedEvent === eventSelected ? 'lightblue' : '',
-              }}
-              onClick={() => setEventSelected( eventSelected === unselectedEvent ? "" : unselectedEvent)}
-            >{unselectedEvent}</Card>
-          )}
+          {genCards()}
         </Col>
       </Row>
     </Container>
