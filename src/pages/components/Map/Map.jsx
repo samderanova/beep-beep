ed08f354fd0c8f1239bb49ca9ac9ceff0141f6bf
Sam Der <sder@uci.edu>
2023-04-23 06:39:28 -0700

    Enable search by city

--- src/pages/components/Map/Map.jsx
+++ src/pages/components/Map/Map.jsx
@@ -19,16 +19,18 @@ import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility
 import exitX from "@/assets/icons/x-lg.svg";
 import Result from "./Result/Result";
 import styles from "./Map.module.scss";
+import search from "@/pages/search";
 
-function CenterAtUserLocation() {
-  const map = useMap();
-  map.locate().on("locationfound", (e) => {
-    map.setView(e.latlng, 12);
-  });
+async function getResults(lat, lon) {
+  const response = await fetch(
+    `/api/location?latitude=${lat}&longitude=${lon}`
+  );
+  const data = await response.json();
+  return data;
 }
 
-function AddMarkerToClick({ setHideModal, setResults }) {
-  const [marker, setMarker] = useState({ lat: 0, lng: 0 });
+function AddMarkerToClick({ setHideModal, setResults, lati = 0, long = 0 }) {
+  const [marker, setMarker] = useState({ lat: lati, lng: long });
 
   const map = useMapEvents({
     async click(e) {
@@ -40,14 +42,6 @@ function AddMarkerToClick({ setHideModal, setResults }) {
     },
   });
 
-  async function getResults(lat, lon) {
-    const response = await fetch(
-      `/api/location?latitude=${lat}&longitude=${lon}`
-    );
-    const data = await response.json();
-    return data;
-  }
-
   useEffect(() => {
     map.locate().on("locationfound", async (e) => {
       setMarker(e.latlng);
@@ -58,11 +52,16 @@ function AddMarkerToClick({ setHideModal, setResults }) {
     map.setView([marker.lat, marker.lng]);
   }, [marker]);
 
+  useEffect(() => {
+    map.setView([lati, long]);
+    setMarker({ lat: lati, lng: long });
+  }, [lati, long]);
+
   return (
     <>
       <Marker position={marker}>
         <Popup>
-          ({marker.lat.toFixed(2)}, {marker.lng.toFixed(2)})
+          ({Number(marker.lat).toFixed(2)}, {Number(marker.lng).toFixed(2)})
         </Popup>
       </Marker>
     </>
@@ -74,6 +73,10 @@ export default function Map() {
   const [hideModal, setHideModal] = useState(true);
   const [center, setCenter] = useState({ lat: 0, lng: 0 });
   const [results, setResults] = useState([]);
+  const [latitude, setLatitude] = useState([]);
+  const [currLat, setCurrLat] = useState(0);
+  const [longitude, setLongitude] = useState([]);
+  const [currLon, setCurrLon] = useState(0);
   const notify = () =>
     toast(
       <>
@@ -94,9 +97,68 @@ export default function Map() {
         lat: position.coords.latitude,
         lng: position.coords.longitude,
       });
+      setCurrLat(position.coords.latitude);
+      setCurrLon(position.coords.longitude);
       setGotLocation(true);
     });
   }, []);
+
+  const [searchEntry, setSearchEntry] = useState("");
+  const [locations, setLocations] = useState([]);
+  const [success, setSuccess] = useState(true);
+  const [l, setL] = useState(0);
+
+  async function submitSearchEntry(e) {
+    e.preventDefault();
+    try {
+      setSearchEntry(e.target.value);
+      const response = await fetch(
+        `https://geocode.maps.co/search?q=${e.target.value}`
+      );
+      const data = await response.json();
+      showSearch(data);
+    } catch {
+      setSuccess(false);
+    }
+  }
+
+  async function showSearch(searchResult) {
+    const locations = [];
+    const lats = [];
+    const lons = [];
+    let l = 0;
+
+    if (searchResult.length > 0) {
+      setSuccess(true);
+      l = searchResult.length;
+
+      if (l > 5) {
+        l = 5;
+      }
+      setL(l);
+      // send max 5 locations
+      for (let i = 0; i < l; i++) {
+        const elipses = false;
+        let s = searchResult[i].display_name;
+        if (s.length > 50) {
+          s = s.substring(0, 50);
+          s = s + "...";
+        }
+
+        locations.push(s);
+        lats.push(searchResult[i].lat);
+        lons.push(searchResult[i].lon);
+      }
+      setLocations(locations);
+      setLatitude(lats);
+      setLongitude(lons);
+    }
+  }
+
+  function updateLatLon(lat, lon) {
+    setCurrLat(lat);
+    setCurrLon(lon);
+  }
 
   return (
     gotLocation && (
@@ -109,6 +171,8 @@ export default function Map() {
           <AddMarkerToClick
             setHideModal={setHideModal}
             setResults={setResults}
+            lati={currLat}
+            long={currLon}
           />
           {results.map((result, index) => (
             <Marker
@@ -136,7 +200,21 @@ export default function Map() {
               boxShadow: "0px 3px 3px gray",
               minWidth: "500px",
             }}
+            value={searchEntry}
+            onChange={(e) => submitSearchEntry(e)}
           />
+          <div>
+            {success ? null : "Error: Please enter valid location."}
+            {locations.map((loc, i) => (
+              <p
+                key={i}
+                id={`loc${i}`}
+                onClick={(e) => updateLatLon(latitude[i], longitude[i])}
+              >
+                {loc}
+              </p>
+            ))}
+          </div>
         </div>
         <div
           className={styles.results + " p-5"}
