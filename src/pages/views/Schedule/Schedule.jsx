727dcc70c1e9bb927ce2db09db3e0543fd0f2ffb
Nathan Huey <nate.huey4@gmail.com>
2023-04-22 23:01:03 -0700

    feat: gen events from db

--- src/pages/views/Schedule/Schedule.jsx
+++ src/pages/views/Schedule/Schedule.jsx
@@ -1,4 +1,8 @@
+import { useState, useEffect } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { Col, Container, Row } from "react-bootstrap";
+import { db } from "@/lib/firebase";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
@@ -6,6 +10,35 @@ import interactionPlugin from "@fullcalendar/interaction";
 import styles from "./Schedule.module.scss";
 
 export default function Schedule() {
+  const auth = getAuth();
+  const [scheduled, setScheduled] = useState([]);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const uid = user.uid;
+        const userRef = doc(db, "users", uid);
+        const docSnap = await getDoc(userRef);
+        setScheduled(docSnap.data().scheduled);
+      } else {
+        console.log("Ooga booga i am not logged in");
+      }
+    });
+  }, []);
+
+  const createEvents = () => {
+    const events = [];
+    for (let i = 0; i < scheduled.length; i++) {
+      const event = {
+        title: scheduled[i].id,
+        start: new Date(scheduled[i].start),
+        end: new Date(scheduled[i].end),
+      };
+      events.push(event);
+    }
+    return events;
+  };
+
   return (
     <Container fluid className={styles.calendar + " p-5"}>
       <Row>
@@ -22,6 +55,7 @@ export default function Schedule() {
             selectable={true}
             selectMirror={true}
             dayMaxEvents={true}
+            events={createEvents()}
           />
         </Col>
         <Col>
